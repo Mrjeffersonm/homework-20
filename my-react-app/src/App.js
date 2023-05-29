253b9abc1d75e707a08181dff2308f1e5e1cd12f
Ryan Pratt <ryan.k.pratt@gmail.com>
2023-05-28 21:05:07 -0700

    Began React functionality

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const App = () => {
+  
+  
+  return (
+    <div className="App">
+      <h1>About Me</h1>
+      <h2>My name is Ryan Pratt, I am a student   </h2>
+    </div>
+  );
+}
+
+export default App;
